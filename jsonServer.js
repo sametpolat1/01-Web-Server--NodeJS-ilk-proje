038750ed28314a8a14e19f88ae915b5b61c7100c
Samet.POLAT <netflix.uyelik84@gmail.com>
2022-03-29 21:35:56 +0300

    first commit

---
+++ jsonServer.js
@@ -0,0 +1,23 @@
+const http = require("http");
+
+const host = "localhost";
+const port = 8000;
+
+const requestListener = function (req, res) {
+    
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+   res.end(`{"message": "This is a JSON response"}`);
+    //res.end(`<html><body><h1>This is HTML</h1></body></html>`);
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, () => {
+
+    
+  console.log(`Server is running on http://${host}:${port}`);
+});
+
+
+
